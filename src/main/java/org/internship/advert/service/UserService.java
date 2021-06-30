c9276dd5af7a072c6374c48839e026c288b7926d
pluser <danchik761@yandex.ru>
2021-07-01 01:00:53 +0300

    Repo try

--- src/main/java/org/internship/advert/service/UserService.java
+++ src/main/java/org/internship/advert/service/UserService.java
@@ -1,8 +1,9 @@
 package org.internship.advert.service;
 
-import org.internship.advert.dao.IGenericDao;
 import org.internship.advert.model.User;
+import org.internship.advert.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,42 +11,45 @@ import java.util.List;
 
 
 @Service
+@ComponentScan("org.internship.advert.repo")
 public class UserService implements IUserService {
 
-    private final IGenericDao<User, Long> userDAO;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserService(IGenericDao<User, Long> userDAO) {
-        this.userDAO = userDAO;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    @Transactional
     public List<User> listUsers() {
-        return this.userDAO.listAll();
+        return this.userRepository.findAll();
     }
 
     @Override
-    @Transactional
     public User addUser(User user) {
-        return this.userDAO.add(user);
+        return this.userRepository.save(user);
     }
 
     @Override
-    @Transactional
-    public User updateUser(User user) {
-        return this.userDAO.update(user);
+    public User updateUser(User updateUser) {
+        return this.userRepository.findById(updateUser.getId())
+                .map(user -> {
+                    user.setEmail(updateUser.getEmail());
+                    user.setFirstName(updateUser.getFirstName());
+                    user.setLastName(updateUser.getLastName());
+                    user.setLogin(updateUser.getLogin());
+                    return this.userRepository.save(user);
+                });
     }
 
     @Override
-    @Transactional
     public void deleteUser(Long id) {
-        this.userDAO.delete(id);
+        this.userRepository.delete(id);
     }
 
     @Override
-    @Transactional
     public User getUserById(Long id) {
-        return this.userDAO.getById(id);
+        return this.userRepository.getById(id);
     }
 }
